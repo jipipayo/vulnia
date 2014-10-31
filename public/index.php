c8158fed49c5544209a72f3ad80366e43e4b1b28
dani <daniel.remeseiro@gmail.com>
2014-10-31 17:37:26 +0100

    now using composer, zf under vendor, removed from library, added resque module

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+// Define application environment
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 
 // Define path to application directory
@@ -6,15 +9,14 @@ defined('APPLICATION_PATH')
 || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 
-// Define application environment
-defined('APPLICATION_ENV')
-|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
                                               realpath(APPLICATION_PATH . '/../library'),
-                                              get_include_path(),
+                                              realpath(APPLICATION_PATH . '/../vendor/zendframework/zendframework1/library')
                                          )));
+
+require_once realpath(APPLICATION_PATH . '/../vendor/autoload.php');
+
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
@@ -24,8 +26,6 @@ $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/conf/application.ini'
 );
-
-
 
 
 $application->bootstrap()->run();
