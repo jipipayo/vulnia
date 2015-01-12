e66f54dcca35b72a9b66e2801d6317456ab7f8d7
dani <daniel.remeseiro@gmail.com>
2015-01-12 23:21:31 +0100

    sphinx now is dynamic via SphinConfigBuilder class

--- resque_config.rb
+++
@@ -1,5 +0,0 @@
-config = ENV.fetch("RAILS_RESQUE_REDIS", "127.0.0.1:6379")
-Resque.redis = config
-# This will make the scheduler tabs show up.
-require 'resque-scheduler'
-require 'resque/scheduler/server'
